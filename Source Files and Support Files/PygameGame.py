06069ea04c256e1fb78737c084767789fdfb97bb
Jason Chang <chihsenc@andrew.cmu.edu>
2017-01-31 20:49:42 -0500

    add all files

---
+++ Source Files and Support Files/PygameGame.py
@@ -0,0 +1,94 @@
+# Name: Chih-Sen (Jason) Chang; Andrew ID: chihsenc; 
+# Section: L; TP Mentor: Josh Korn
+# Pygame Framework created by Lukas Peraza for the 15-112 F15 Pygame Lecture
+
+import pygame
+
+class PygameGame(object):
+    # FRAMEWORK CREATED BY LUKAS PERAZA
+
+    def init(self):
+        pass
+
+    def mousePressed(self, x, y):
+        pass
+
+    def mouseReleased(self, x, y):
+        pass
+
+    def mouseMotion(self, x, y):
+        pass
+
+    def mouseDrag(self, x, y):
+        pass
+
+    def keyPressed(self, keyCode, modifier):
+        pass
+
+    def keyReleased(self, keyCode, modifier):
+        pass
+
+    def timerFired(self, dt):
+        pass
+
+    def redrawAll(self, screen):
+        pass
+
+    def isKeyPressed(self, key):
+        ''' return whether a specific key is being held '''
+        return self._keys.get(key, False)
+
+    def __init__(self, width=900, height=600, fps=10, title="Silent Hill"):
+        self.width = width
+        self.height = height
+        self.fps = fps
+        self.title = title
+        self.bgColor = (255, 255, 255)
+        pygame.init()
+
+    def run(self):
+
+        clock = pygame.time.Clock()
+        screen = pygame.display.set_mode((self.width, self.height))
+        # set the title of the window
+        pygame.display.set_caption(self.title)
+
+        # stores all the keys currently being held down
+        self._keys = dict()
+
+        # call game-specific initialization
+        # The preInit() function initializes only once when the game is first 
+        # launched because it doesn't need to be unnecessarily loaded every time
+        # the game is over or the game restarts. Even though it is still slow 
+        # in the beginning when it loads for the first time, it won't be slow 
+        # whenever init() needs to be called again.
+        self.preInit()
+        self.init()
+        playing = True
+        while playing:
+            time = clock.tick(self.fps)
+            self.timerFired(time)
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    self.mousePressed(*(event.pos))
+                elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                    self.mouseReleased(*(event.pos))
+                elif (event.type == pygame.MOUSEMOTION and
+                      event.buttons == (0, 0, 0)):
+                    self.mouseMotion(*(event.pos))
+                elif (event.type == pygame.MOUSEMOTION and
+                      event.buttons[0] == 1):
+                    self.mouseDrag(*(event.pos))
+                elif event.type == pygame.KEYDOWN:
+                    self._keys[event.key] = True
+                    self.keyPressed(event.key, event.mod)
+                elif event.type == pygame.KEYUP:
+                    self._keys[event.key] = False
+                    self.keyReleased(event.key, event.mod)
+                elif event.type == pygame.QUIT:
+                    playing = False
+            screen.fill(self.bgColor)
+            self.redrawAll(screen)
+            pygame.display.flip()
+
+        pygame.quit()
---
+++ readme.txt
@@ -0,0 +1,15 @@
+Readme
+
+My 15-112 Fall 2015 term project is a survival game that is based off the game Silent Hill. The horror game starts with the player in one room that has potions, gold, ammo, and doors that lead to other rooms. In each wave, monsters will come out and try to kill the player. The player’s main objective is to survive by maintaining health and sanity while looking for ammo to shoot monsters and gold to build walls.
+
+My program’s programming language is Python 3. The main module it uses is Pygame, which is not pre-installed in python. 
+
+To install on Mac, go to this link:
+http://florian-berger.de/en/articles/installing-pygame-for-python-3-on-os-x/
+To install on Windows, go to this link (courtesy of Rohan Varma):
+https://docs.google.com/document/d/1A-FudgG9rdL-qcuGavGJ5Z9L7fW_nUQg9WuL6CwTs_Y/edit
+The Pygame packages are available for download on the Pygame website:
+http://www.pygame.org/download.shtml
+
+Other modules that are used include, random, math, and datetime. These are pre-installed in python. 
+To run the game, open Game.py in the Source Files and Support Files folder and you run the python file (Ctrl + B on Windows and Command + B on Mac).
